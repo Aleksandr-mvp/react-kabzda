977bcfe58efd408371f84dd1e136d43858e20a7f
Drogon <deadreebook@gmail.com>
2021-12-17 19:44:53 +0700

    added useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
 
 function App() {
     return (
         <div>
-            <Accordion titleValue={'Main Menu'} collapsed={true}/>
-            <Accordion titleValue={'My List'} collapsed={false}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <OnOff />
+            <OnOff />
+            <OnOff />
+            {/*<OnOff />*/}
+            {/*<OnOff />*/}
+            {/*<Accordion titleValue={'Main Menu'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'My List'} collapsed={false}/>*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
         </div>
     )
 }
