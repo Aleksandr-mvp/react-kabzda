1738030020f0c6bb388cc9e10227f9cf1a9c1641
Aleksandr <deadreebook@gmail.com>
2021-12-24 14:08:25 +0700

    added RatingStar

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 
 function App() {
+
+   let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className={'App'}>
             <OnOff />
 
-            <UncontrolledAccordion titleValue={'Menu'} />
+            {/*<UncontrolledAccordion titleValue={'Menu'} />*/}
 
-            <UncontrolledRating />
+            {/*<UncontrolledRating />*/}
 
             {/*<Accordion titleValue={'Menu'} collapsed={false} />*/}
-            {/*<Rating value={3} />*/}
-
-            {/*<Rating value={0}/>*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={2}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
+            <Rating value={ratingValue} onClick={setRatingValue} />
         </div>
     )
 }
